bc057ff76cc15538390ca102c81d3db31c8c76bd
Thornton Thomas L <Thomas.Thornton@srpnet.com>
2020-07-31 14:31:04 -0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { PostComponent } from './post/post.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    PostComponent,
+    SidebarComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
